828dea5d38b96fa5d4661f1246a6414614d99dd2
farhan adyatama <frhanadyatama17@gmail.com>
2023-01-26 14:11:08 +0800

    new login

--- resources/views/Login/login.blade.php
+++ resources/views/Login/login.blade.php
@@ -29,23 +29,29 @@
   <link href="css/style.css" rel="stylesheet">
 </head>
 
-  <body>
+  <body class="bg-light">
     <div class="row justify-content-center">
-      <div class="col-md-4">
+      <div class="col-lg-5">
+
         @if(session()->has('success'))
         <div class="alert alert-success alert-dismissible fade show" role="alert">
           {{ session('success') }}
           <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
         @endif
+
         @if(session()->has('loginError'))
         <div class="alert alert-danger alert-dismissible fade show" role="alert">
           {{ session('loginError') }}
           <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
         @endif
+
         <main class="form-signin w-100 m-auto">
+          <img class="mb-4 mt-4 background-center mx-auto d-block" src="/img/logo_putr.png" alt="logo_putr" width="125">
+
           <h1 class="h3 mb-3 fw-normal text-center">Please Login</h1>
+
           <form action="/login.login" method="post">
             @csrf
             <div class="form-floating">
@@ -55,12 +61,15 @@
               <div class="invalid-feedback">{{ $message }}</div>
               @enderror
             </div>
+
             <div class="form-floating">
               <input type="password" name="password" class="form-control" id="password" placeholder="Password" required>
               <label for="password">Password</label>
             </div>
-            <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
+
+            <button class="w-100 btn btn-lg btn-primary mt-3" type="submit">Login</button>
           </form>
+          
           <small class="d-block text-center mt-3">Not Registered? <a href="/register">Register Now!</a></small>
         </main>
       </div>
--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -29,11 +29,14 @@
   <link href="css/style.css" rel="stylesheet">
 </head>
 
-  <body>
+  <body class="bg-light">
     <div class="row justify-content-center">
           <div class="col-lg-5">
+
             <main class="form-registration w-100 m-auto">
+              <img class="mb-4 mt-4 mx-auto d-block" src="/img/logo_putr.png" alt="logo_putr" width="125">
               <h1 class="h3 mb-3 fw-normal text-center">Registration Form</h1>
+
               <form action="/register" method="post">
                 @csrf
                 <div class="form-floating">
@@ -43,6 +46,7 @@
                   <div class="invalid-feedback">{{ $message }}</div>
                   @enderror
                 </div>
+
                 <!-- <div class="form-floating">
                   <input type="text" name="username" class="form-control rounded-top @error('username')is-invalid @enderror" id="username" placeholder="username" required value="{{ old('username') }}">
                   <label for="name">Username</label>
@@ -50,6 +54,7 @@
                   <div class="invalid-feedback">{{ $message }}</div>
                   @enderror
                 </div> -->
+
                 <div class="form-floating">
                   <input type="email" name="email" class="form-control @error('email')is-invalid @enderror" id="email" placeholder="name@example.com" required value="{{ old('email') }}">
                   <label for="email">Email address</label>
@@ -57,6 +62,7 @@
                   <div class="invalid-feedback">{{ $message }}</div>
                   @enderror
                 </div>
+
                 <div class="form-floating">
                   <input type="password" name="password" class="form-control rounded-bottom @error('password')is-invalid @enderror" id="password" placeholder="Password">
                   <label for="password">Password</label>
@@ -64,6 +70,7 @@
                   <div class="invalid-feedback">{{ $message }}</div>
                   @enderror
                 </div>
+                
                 <button class="w-100 btn btn-lg btn-primary mt-3" type="submit">Register</button>
               </form>
               <small class="d-block text-center mt-3">Already Registered? <a href="/login.login">Login</a></small>
